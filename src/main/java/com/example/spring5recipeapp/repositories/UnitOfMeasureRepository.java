cdb84db58bf6073c4fc69fb81aebdcbc85444d91
Nikola Martić <nikola.martic@gmail.com>
2019-04-03 16:52:16 +0200

    Created Spring Data Repositories

---
+++ src/main/java/com/example/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,9 @@
+package com.example.spring5recipeapp.repositories;
+
+import com.example.spring5recipeapp.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+
+}
