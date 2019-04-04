0a0bfef93393446b9b0d07eced42f5fddc8639d5
Nikola Martić <nikola.martic@gmail.com>
2019-04-04 11:04:06 +0200

    List of Recipes is now displayed on index page

--- src/main/java/com/example/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/example/spring5recipeapp/controllers/IndexController.java
@@ -1,35 +1,24 @@
 package com.example.spring5recipeapp.controllers;
 
-import com.example.spring5recipeapp.domain.Category;
-import com.example.spring5recipeapp.domain.UnitOfMeasure;
-import com.example.spring5recipeapp.repositories.CategoryRepository;
-import com.example.spring5recipeapp.repositories.UnitOfMeasureRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
-
-    @Autowired
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    private final RecipeService recipeService;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
 
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        model.addAttribute("recipes", recipeService.getRecipes());
 
-        System.out.println("Category Id is:\t" + categoryOptional.get().getId());
-        System.out.println("UOM Id is:\t" + unitOfMeasureOptional.get().getId());
 
 
         return "index";
