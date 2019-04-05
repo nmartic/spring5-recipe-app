f633714ab05604eda12acdc8cbc1f9175933f62b
Nikola Martić <nikola.martic@gmail.com>
2019-04-05 14:04:02 +0200

    Bug fixes

--- src/main/java/com/example/spring5recipeapp/domain/Category.java
+++ src/main/java/com/example/spring5recipeapp/domain/Category.java
@@ -2,11 +2,13 @@ package com.example.spring5recipeapp.domain;
 
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Category {
 
--- src/main/java/com/example/spring5recipeapp/domain/Notes.java
+++ src/main/java/com/example/spring5recipeapp/domain/Notes.java
@@ -2,10 +2,12 @@ package com.example.spring5recipeapp.domain;
 
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
 
