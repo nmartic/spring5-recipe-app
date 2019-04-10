d7869b1b8341936920360ce737e72d9b7b2338fb
Nikola Martić <nikola.martic@gmail.com>
2019-04-10 14:42:56 +0200

    Added - Update a Recipe

--- src/main/java/com/example/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/example/spring5recipeapp/services/RecipeServiceImpl.java
@@ -48,6 +48,14 @@ public class RecipeServiceImpl implements RecipeService {
         return recipeOptional.get();
     }
 
+
+    @Override
+    @Transactional
+    public RecipeCommand findCommandById(Long l) {
+        return recipeToRecipeCommand.convert(findById(l));
+    }
+
+
     @Override
     @Transactional
     public RecipeCommand saveRecipeCommand(RecipeCommand command) {
--- src/test/java/com/example/spring5recipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/com/example/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.example.spring5recipeapp.services;
 
+import com.example.spring5recipeapp.commands.RecipeCommand;
 import com.example.spring5recipeapp.converters.RecipeCommandToRecipe;
 import com.example.spring5recipeapp.converters.RecipeToRecipeCommand;
 import com.example.spring5recipeapp.domain.Recipe;
@@ -52,6 +53,27 @@ public class RecipeServiceImplTest {
         Recipe recipeReturned = recipeService.findById(1L);
 
         assertNotNull("Null recipe returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+
+    @Test
+    public void getRecipeCoomandByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        when(recipeToRecipeCommand.convert(any())).thenReturn(recipeCommand);
+
+        RecipeCommand commandById = recipeService.findCommandById(1L);
+
+        assertNotNull("Null recipe returned", commandById);
         verify(recipeRepository, times(1)).findById(anyLong());
         verify(recipeRepository, never()).findAll();
     }
